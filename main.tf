0bae58893c74fec57240801c177e0dc7f6ac39a2
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-11-13 12:32:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "vm" {
     machine_type = "e2-micro"
     boot_disk {
       initialize_params {
-        image = "ubuntu-os-cloud/ubuntu-2204-lts-amd64"
+        image = "ubuntu-os-cloud/ubuntu-2204-lts"
       }
     }
     network_interface {
